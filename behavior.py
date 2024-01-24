5119f79f9ea4e9ec12749f225fdcd6ca2022009f
kim3722 <kim3722@purdue.edu>
2024-01-23 20:26:04 -0500

    big fsm done

--- behavior.py
+++ behavior.py
@@ -1,77 +1,171 @@
-class behavior_flage:
+import rospy
+import smach
+
+class ascent(smach.State):
     def __init__(self):
-        self.plan_to_mining_fl = True
-        self.goto_mining_fl = False
-        self.do_mining_fl = False
-        self.plan_to_berm_fl = False
-        self.goto_berm_fl = False
-        self.deposit_fl = False
-        self.dcs_ost_extrustio_fl = False
-        self.adjust_goal_point_fl = False
-        self.identify_obsticl_fl = False
-        self.identify_obsticle_fl = False
-        self.retrace_path_fl = False
+        smach.State.__init__(self, 
+                outcomese=['init mapping', 'traversal to mining', 'traversal to berm', 'fail'])
+                
+    
+    def execute (self, behavior):
+        # extend linear actuators
+        if True:
+            return 'pass'
+        else:
+            return 'fail'
+        
+class init_mapping(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+                outcomes=['pass', 'fail'])
+    
+    def execute (self, behavior):
+        # sping 360 & find april tag & set target
 
-    def del_fl(self):
-        self.plan_to_mining_fl = False
-        self.goto_mining_fl = False
-        self.do_mining_fl = False
-        self.plan_to_berm_fl = False
-        self.goto_berm_fl = False
-        self.deposit_fl = False
-        self.dcs_ost_extrustio_fl = False
-        self.adjust_goal_point_fl = False
-        self.identify_obsticl_fl = False
-        self.identify_obsticle_fl = False
-        self.retrace_path_fl = False
-# path plan to mining
-def plan_to_mining(is_planning_failed):
-    while (is_planning_failed):
-        # Decrease obstacle extrusion
+        # generatte trajectory
 
-    else:
-        # go to mining area
+        if True:
+            return 'pass'
+        else:
+            return 'fail'
 
-def goto_mining(is_robot_stuck, is_map_changed):
-    if (is_robot_stuck):
-        # Identify obsiticle
-    elif (is_map_changed):
-        # stop moving & back to pathplan_to_mining
-    else:
-        # at mining area & do mining
+class traversal_to_mining(smach.State):
+    def __init__(self):
+        smach.State.__inti__(self,
+                outcomes=['pass', 'fail'])
+        
+    def execute (self, behavior):
+        # generate trajectory
 
-def do_mining():
-    # be sad & need more detail
+        # move along trajectory + recieve sensor input(imu, uwb, images) + update map
 
-def plan_to_berm(is_planning_failed):
-    while is_planning_failed:
-        # Decrease obstacle extrusion
-    else:
-        #  go to berm
+        if True:
+            return 'pass'
+        else:
+            return 'fail'
+        
+class traversal_to_berm(smach.State):
+    def __init__(self):
+        smach.State.__inti__(self,
+                outcomes=['target reached', 'detect robot stuck'])
+        
+    def execute (self, behavior):
+        # generate trajectory
 
-def goto_berm(is_robot_stuck, is_map_changed):
-    if (is_robot_stuck):
-        # Identify obsiticle
-    elif (is_map_changed):
-        # stop moving & back to pathplan_to_mining
-    else:
-        # deposit
+        # move along trajectory + recieve sensor input(imu, uwb, images) + update map
 
-def deposit():
-    while (is_deposit_failed):
+        if True:
+            return 'pass'
+        else:
+            return 'fail'
+        
+class plunging(smach.Sate):
+    def __init__(self):
+        smach.State.__init__(self,
+                outcomes=['pass', 'fail'])
+    
+    def execute (self, behavior):
+        # Set excavation to full speed + lower 90% of distance
 
-    # bin empty & goto plan to mining
+        # reduce speed to 25%
 
-def dcs_ost_extrustion():
-    if (is_decreased):
-        # adj goal point
+        if True:
+            return 'pass'
+        else:
+            return 'fail'
 
-def adjust_goal_point():
+class trenching(smach.Sate):
+    def __init__(self):
+        smach.State.__init__(self,
+                outcomes=['load cell full', 'Obstacle reached', 'fail'])
+    
+    def execute (self, behavior):
+        # drive forward + adjust speed
 
-def identify_obsticle():
-    if (is_crater):
-        # wiggle
-    else:
-        # retrace_path
+        if True:
+            return 'pass'
+        else:
+            return 'fail'
+        
+class deposit(smach.Sate):
+    def __init__(self):
+        smach.State.__init__(self,
+                outcomes=['load cell empty', 'fail':'FUCKED'])
+    
+    def execute (self, behavior):
+        # spin auger
 
-def retrace_path():
+        # stop spinning auger
+
+        if True:
+            return 'pass'
+        else:
+            return 'fail'
+        
+class post_deposit(smach.Sate):
+    def __init__(self):
+        smach.State.__init__(self,
+                outcomes=['pass', 'fail'])
+    
+    def execute (self, behavior):
+        # shift future deposition location
+
+        if True:
+            return 'pass'
+        else:
+            return 'fail'
+        
+class escape(smach.Sate):
+    def __init__(self):
+        smach.State.__init__(self,
+                outcomes=['back to traversal to mining', 'back to traversal to berm', 'detect robot stuck':'ESCAPE'])
+    
+    def execute (self, behavior):
+        # drive stright at 100% speed
+
+        if True:
+            return 'pass'
+        else:
+            return 'fail'
+
+
+def main():
+    rospy.init_node('smach_example_state_machine')
+
+    sm = smach.StateMachine(outcomes = ['PASS', 'FAIL'])
+
+    with sm:
+        smach.StateMachine.add('ASCENT', ascent(), 
+                               transitions={'init mapping':'INIT_MAPPING',
+                                            'traversal to mining':'TRAVERSAL_TO_MINING',
+                                            'traversal to berm':'TRAVERSAL_TO_BERM',
+                                             'fail':'FUCKED'})
+        smach.StateMachine.add('INIT_MAPPING', init_mapping(), 
+                               transitions={'pass':'TRAVERSAL_TO_MINING', 
+                                             'fail':'FUCKED'})
+        smach.StateMachine.add('TRAVERSAL_TO_MINING', traversal_to_mining(),
+                               transitions={'pass':'PLUGING',
+                                             'fail':'ESCAPE'})
+        smach.StateMachine.add('PLUGING', plunging(), 
+                               transitions={'pass':'TRENCHING', 
+                                             'fail':'FUCKED'})
+        smach.StateMachine.add('TRENCHING', trenching(), 
+                               transitions={'load cell full':'ASCENT',
+                                            'Obstacle reached':'ASCENT', 
+                                             'fail':'FUCKED'})
+        smach.StateMachine.add('TRAVERSAL_TO_BERM', traversal_to_berm(), 
+                               transitions={'target reached':'DEPOSIT', 
+                                            'detect robot stuck':'ESCAPE'})
+        smach.StateMachine.add('DEPOSIT', deposit(), 
+                               transitions={'load cell empty':'POST_DEPOSIT', 
+                                            'fail':'FUCKED'})
+        smach.StateMachine.add('POST_DEPOSIT', post_deposit(), 
+                               transitions={'pass':'ASCENT', 
+                                            'fail':'FUCKED'})
+        smach.StateMachine.add('ESCAPE', escape(), 
+                               transitions={'back to traversal to mining':'TRAVERSAL_TO_MINING',
+                                            'back to traversal to berm':'TRAVERSAL_TO_BERM', 
+                                            'detect robot stuck':'ESCAPE'})
+        
+        
+        
